e2cc03b8ef9f6df8f6bbcc5efcf3ee0682262d4d
homayon <homayon623@gmail.com>
2020-02-11 02:13:57 -0800

    van con is add

--- vehicle_info/van.cs
+++ vehicle_info/van.cs
@@ -8,6 +8,17 @@ namespace Vehicle_info
     {
         public van()
         {
+            this.brand = null;
+            this.model = null;
+            this.car_tag = null;
+            this.fuel_type = "Petrol";
+            this.color = null;
+            // how to make null int 
+            this.wheel_number = 4;
+            this.Axis_number = 1;
+            this.Cylinders_number = 4;
+            this.Passenger_number = 9;
+            this.Base = 1;
 
         }
     }
